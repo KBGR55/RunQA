0a04b86ac7fb4e814b7eb563a5a829c9bf648789
Hilary-Madelein <madeleycc15@gmail.com>
2025-02-05 08:21:42 -0500

    Cambio en init-db

--- mysql-init/init-db.sql
+++ mysql-init/init-db.sql
@@ -172,7 +172,7 @@ CREATE TABLE IF NOT EXISTS peticion (
 );
 
 
-START TRANSACTION;
+     START TRANSACTION;
 
      INSERT INTO rol (id, external_id, estado, nombre, createdAt, updatedAt) VALUES      
      (1, '1376cf7e-907c-11ef-8f4d-30e37a2aa82d', 1, 'ADMINISTRADOR SYS', '2024-10-19 05:30:36', '2024-10-19 05:30:36'),
@@ -183,8 +183,8 @@ START TRANSACTION;
 
      INSERT INTO entidad (id, external_id, estado, foto, nombres, apellidos, fecha_nacimiento, telefono, createdAt, updatedAt) 
      VALUES 
-     (1, '93a9e3f1-907c-11ef-8f4d-30e37a2aa82d', 1, '3efe8462-5255-4839-b694-c269ca4475b3.jpeg', 'KAREN BRIGITH', 'GONZAGA RIVAS', '2003-12-05 00:00:00', '0980715353', '2024-10-22 08:50:19', '2024-10-22 08:50:19'),
-     (2, '93a9e97e-907c-11ef-8f4d-30e37a2aa82d', 1, '2307fd96-9917-4da1-a666-90d0711162c3.jpeg', 'HILARY MADELEY', 'CALVA CAMACHO', '1995-08-15 00:00:00', '0987654321', '2024-10-22 08:50:19', '2024-10-22 08:50:19'),
+     (1, '93a9e3f1-907c-11ef-8f4d-30e37a2aa82d', 1, 'USUARIO_ICONO.png', 'ADMIN', 'SISTEMA', '2003-12-05 00:00:00', '0980715353', '2024-10-22 08:50:19', '2024-10-22 08:50:19'),
+     (2, '93a9e97e-907c-11ef-8f4d-30e37a2aa82d', 1, 'USUARIO_ICONO.png', 'HILARY MADELEY', 'CALVA CAMACHO', '1995-08-15 00:00:00', '0987654321', '2024-10-22 08:50:19', '2024-10-22 08:50:19'),
      (3, '93a9eb2d-907c-11ef-8f4d-30e37a2aa82d', 1, 'USUARIO_ICONO.png', 'MARIA ELENA', 'PEREZ MARTINEZ', '1998-03-22 00:00:00', '0980123456', '2024-10-22 08:50:19', '2024-10-22 08:50:19'),
      (4, '25b65566-907d-11ef-8f4d-30e37a2aa82d', 1, 'USUARIO_ICONO.png', 'PEDRO ANTONIO', 'RAMIREZ VARGAS', '1987-07-30 00:00:00', '0998765432', '2024-10-22 08:50:19', '2024-10-22 08:50:19'),
      (5, '93a9ed2c-907c-11ef-8f4d-30e37a2aa82d', 1, 'USUARIO_ICONO.png', 'ANA MARIA', 'TORRES QUINTERO', '2000-11-10 00:00:00', '0976543210', '2024-10-22 08:50:19', '2024-10-22 08:50:19'),
@@ -193,7 +193,7 @@ START TRANSACTION;
 
      INSERT INTO cuenta (id, external_id, estado, correo, clave, createdAt, updatedAt, id_entidad) 
      VALUES 
-     (1, '594760f1-907e-11ef-8f4d-30e37a2aa82d', 1, 'karen.b.gonzaga@unl.edu.ec', '$2a$08$vcbwdzAoBjH027Yt6B9PwO3G65afLhrMfejne1EJ7uoPGuLslHLC6', '2024-10-22 09:03:00', '2024-10-22 09:03:00', 1),
+     (1, '594760f1-907e-11ef-8f4d-30e37a2aa82d', 1, 'admin.admin@unl.edu.ec', '$2a$08$vcbwdzAoBjH027Yt6B9PwO3G65afLhrMfejne1EJ7uoPGuLslHLC6', '2024-10-22 09:03:00', '2024-10-22 09:03:00', 1),
      (2, '59476a5b-907e-11ef-8f4d-30e37a2aa82d', 1, 'hilary.calva@unl.edu.ec', '$2a$08$vcbwdzAoBjH027Yt6B9PwO3G65afLhrMfejne1EJ7uoPGuLslHLC6', '2024-10-22 09:03:00', '2024-10-22 09:03:00', 2),
      (3, '59476cb2-907e-11ef-8f4d-30e37a2aa82d', 1, 'maria.perez@unl.edu.ec', '$2a$08$vcbwdzAoBjH027Yt6B9PwO3G65afLhrMfejne1EJ7uoPGuLslHLC6', '2024-10-22 09:03:00', '2024-10-22 09:03:00', 3),
      (4, '59476e19-907e-11ef-8f4d-30e37a2aa82d', 1, 'pedro.ramirez@unl.edu.ec', '$2a$08$vcbwdzAoBjH027Yt6B9PwO3G65afLhrMfejne1EJ7uoPGuLslHLC6', '2024-10-22 09:03:00', '2024-10-22 09:03:00', 4),
@@ -223,7 +223,8 @@ START TRANSACTION;
 
      INSERT INTO caso_prueba (external_id, nombre, estado, estadoAsignacion, descripcion, precondiciones, datos_entrada, pasos, resultado_esperado, clasificacion, tipo_prueba, fecha_disenio, fecha_ejecucion_prueba, createdAt, updatedAt, id_proyecto)
      VALUES
-     (UUID(), 'Validacion de Inputs en Formularios', 'DISENADO', 'NO ASIGNADO', 'Verificacion de datos de entrada en formularios.', 'El formulario debe estar activo.', 'Entrada de datos validos.', 'Paso 1: Abrir formulario.\nPaso 2: Ingresar datos.', 'El sistema valida los datos correctamente.', 'MEDIA', 'FUNCIONAL', NOW(), NULL, NOW(), NOW(), 1),
-     (UUID(), 'Prueba de Conexion de Enlaces', 'DISENADO', 'NO ASIGNADO', 'Validacion de enlaces en el sistema.', 'Sistema operativo configurado.', 'Enlace a servidores configurados.', 'Paso 1: Configurar red.\nPaso 2: Ejecutar enlace.', 'El sistema establece conexion correctamente.', 'ALTA', 'INTEGRACION', NOW(), NULL, NOW(), NOW(), 2);
+     (UUID(), 'Validación de Inputs en Formularios', 'DISENADO', 'NO ASIGNADO', 'Verificacion de datos de entrada en formularios.', 'El formulario debe estar activo.', 'Entrada de datos validos.', 'Paso 1: Abrir formulario.\nPaso 2: Ingresar datos.', 'El sistema valida los datos correctamente.', 'MEDIA', 'FUNCIONAL', NOW(), NULL, NOW(), NOW(), 1),
+     (UUID(), 'Prueba de Conexion de Enlaces', 'DISENADO', 'NO ASIGNADO', 'Validacion de enlaces en el sistema.', 'Sistema operativo configurado.', 'Enlace a servidores configurados.', 'Paso 1: Configurar red.\nPaso 2: Ejecutar enlace.', 'El sistema establece conexión correctamente.', 'ALTA', 'INTEGRACION', NOW(), NULL, NOW(), NOW(), 2);
 
      COMMIT;
+
